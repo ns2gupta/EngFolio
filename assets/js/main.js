d82f002306da3afc13aa05b53f3d7e225e8b1270
ns2gupta <namangupta2810@gmail.com>
2021-05-02 10:59:55 -0400

    Resume added

--- assets/js/main.js
+++ assets/js/main.js
@@ -124,7 +124,22 @@
       backSpeed: 30
     });
   }
-
+/**
+   * Skills animation
+   */
+  let skilsContent = select('.skills-content');
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: '80%',
+      handler: function(direction) {
+        let progress = select('.progress .progress-bar', true);
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%'
+        });
+      }
+    })
+  }
   /*--/ Testimonials owl /--*/
   $('#testimonial-mf').owlCarousel({
     margin: 20,
